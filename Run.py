42ff40165d32c5391a782d01dca8d3ff6bc755a8
aminreza <108486211+artorki@users.noreply.github.com>
2022-11-23 21:01:41 +0330

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,5 +1,9 @@
+@artorki
+
+
 from colorama import Fore as color
 from time import sleep
+
 from Banner import banner
 from URLToIP import url_ip
 from CloudFlare import cloud_flare
@@ -8,47 +12,48 @@ from PortScanner import port_scanner
 from Whois import whois
 
 
-while True:
-    banner()
+while True :
 
-    print(color.LIGHTYELLOW_EX,"–•[HOME]•–———————————————————————————————————————————————————————————")
-    sleep(0.05)
-    print(color.LIGHTYELLOW_EX,"\n [01] URL To IP")
-    sleep(0.05)
-    print(color.LIGHTYELLOW_EX,"[02] Cloud Flare")
-    sleep(0.05)
-    print(color.LIGHTYELLOW_EX,"[03] IP Location")
-    sleep(0.05)
-    print(color.LIGHTYELLOW_EX,"[04] Port Sccaner")
-    sleep(0.05)
-    print(color.LIGHTYELLOW_EX,"[05] Whois")
-    sleep(0.05)
-    print(color.LIGHTYELLOW_EX,"[00] Exit")
-    sleep(0.05)
-    print(color.WHITE)
-    h_input = input(" @G.O>>> ")
+    banner ()
+
+    print (color.LIGHTYELLOW_EX , "–•[HOME]•–———————————————————————————————————————————————————————————")
+    sleep (0.05)
+    print (color.LIGHTYELLOW_EX , "\n [01] URL To IP")
+    sleep (0.05)
+    print (color.LIGHTYELLOW_EX , "[02] Cloud Flare")
+    sleep (0.05)
+    print (color.LIGHTYELLOW_EX , "[03] IP Location")
+    sleep (0.05)
+    print (color.LIGHTYELLOW_EX , "[04] Port Sccaner")
+    sleep (0.05)
+    print (color.LIGHTYELLOW_EX , "[05] Whois")
+    sleep (0.05)
+    print (color.LIGHTYELLOW_EX,"[00] Exit")
+    sleep (0.05)
+    print (color.WHITE)
+    h_input = input (" @G.O>>> ")
 
 
-    if h_input == "1" :
-        url_ip()
+    if h_input == "1" or h_input == "01" :
+        url_ip ()
 
-    elif h_input == "2" :
-        cloud_flare()
+    elif h_input == "2" or h_input == "02" :
+        cloud_flare ()
 
-    elif h_input == "3" :
-        ip_location()
+    elif h_input == "3" or h_input == "03" :
+        ip_location ()
 
-    elif h_input == "4" :
-        port_scanner()
+    elif h_input == "4" or h_input == "04" :
+        port_scanner ()
 
-    elif h_input == "5" :
-        whois()
+    elif h_input == "5" or h_input == "05" :
+        whois ()
 
     elif h_input == "0" :
-        exit()
+        exit ()
 
     else:
-        print(color.LIGHTRED_EX,"\n ERROR")
-        exit()
+        print (color.LIGHTRED_EX , "\n ERROR")
+        exit ()
 
-    input()
+    input ()
