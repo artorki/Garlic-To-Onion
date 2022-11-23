554a8a84016e6f29332f494891d3515def81766a
aminreza <108486211+artorki@users.noreply.github.com>
2022-11-23 21:35:01 +0330

    Update CloudFlare.py

--- CloudFlare.py
+++ CloudFlare.py
@@ -1,51 +1,62 @@
+# Garlic To Onion / Cloud Flare
+
+
 from ipapi import location
 from time import sleep
 from socket import gethostbyname
 from colorama import Fore
+
 from Banner import banner
 
 
 ly,lr,lg,g,w = Fore.LIGHTYELLOW_EX,Fore.LIGHTRED_EX,Fore.LIGHTGREEN_EX,Fore.GREEN,Fore.WHITE
 
 
-def cloud_flare():
+def cloud_flare() :
+    
     banner()
 
-    sleep(0.05)
-    print(ly,"–•[Cloud Flare]•–———————————————————————————————————————————————————————————")
+    sleep (0.05)
+    print (ly , "–•[Cloud Flare]•–———————————————————————————————————————————————————————————")
 
-    sleep(0.05)
-    print(ly,"\n [!] ENTER A URL:")
+    sleep (0.05)
+    print (ly , "\n [~] ENTER A URL:")
 
-    sleep(0.05)
-    print(w)
-    url = input(" @G.O>>> ").lower()
+    
+    sleep (0.05)
+    print (w)
+    url = input(" @G.O>>> ") . lower()
 
     if url == "0" :
         exit()
+        
     elif url == "" :
-        print(lr,"\n ERROR")
+        print (lr , "\n [!] ERROR")
         exit()
 
 
     sub_list = ["home","admin","administrator","blog","blogs","shop","downloads","webmail","mail","email","mysql","local","localhost","cpanel","ftp","direct","forums","www","www1","www2","www3","www4","www5","api","server","vpn"]
 
-    try:
-        sleep(0.05)
-        print(g)
-        print(" [+] " + gethostbyname(str(url)) + " : " + url , lg)
-    except:
-        print(lr,"\n Error")
+    try :
+        sleep (0.05)
+        print (g)
+        print (" [+] " + gethostbyname(str(url)) + " : " + url , lg)
+        
+    except :
+        print (lr , "\n [!] Error")
         exit()
 
+        
     for i in sub_list :
 
-        try:
+        try :
             host = str(i) + "." + url
             ip = gethostbyname(str(host))
             loc = location(ip)
-            print(" [+] " + loc["country_name"] + " : " + ip + " : " + host)
+            print (" [+] " + loc["country_name"] + " : " + ip + " : " + host)
+            
         except:
             pass
     
-    print(ly,"\n Finish")
+    
+    print (ly , "\n Finish")
