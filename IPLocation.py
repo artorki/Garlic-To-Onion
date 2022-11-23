ac171748791e28e60f4393745d334aef2891ce70
aminreza <108486211+artorki@users.noreply.github.com>
2022-11-23 20:53:44 +0330

    Add files via upload

---
+++ IPLocation.py
@@ -0,0 +1,65 @@
+from time import sleep
+from colorama import Fore
+from ipapi import location
+from Banner import banner
+
+
+ly,lr,g,lg,w = Fore.LIGHTYELLOW_EX,Fore.LIGHTRED_EX,Fore.GREEN,Fore.LIGHTGREEN_EX,Fore.WHITE
+
+
+def ip_location():
+    banner()
+
+    sleep(0.05)
+    print(ly,"–•[IP LOCATION]•–———————————————————————————————————————————————————————————")
+
+    sleep(0.05)
+    print(ly,"\n [!] ENTER A IP:")
+
+    sleep(0.05)
+    print(w)
+    ip = input(" @G.O>>> ")
+
+    if ip == "0" :
+        exit()
+    elif ip == "" :
+        print(lr,"\n ERROR")
+        exit()
+
+    try:
+        loc = location(ip)
+
+        print(g,"\n [+] IP:")
+        print(lg,"IP: " + loc["ip"])
+        print(" Network: " + loc["network"])
+        print(" Version: " + loc["version"])
+
+        print(g,"[+] Location:")
+        print(lg,"Country: " + loc["country_name"])
+        print(" Center: " + loc["country_capital"])
+        print(" City: " + loc["city"])
+        print(" Region: " + loc["region"])
+
+        print(g,"[+] Country Information:")
+        print(lg,"Languages: " + loc["languages"])
+        print(" Timezone: " + loc["timezone"])
+        print(" Calling Code: " + loc["country_calling_code"])
+        print(" Utc Offset: " + loc["utc_offset"])
+        print(" Country Tld: " + loc["country_tld"])
+        print(" Currency: " + loc["currency"])
+        print(" Currency Name: " + loc["currency_name"])
+        print(" Region Code: " + loc["region_code"])
+        print(" Country Code: " + loc["country_code"])
+        print(" Country Code iso3: " + loc["country_code_iso3"])
+        print(" Continent Code: " + loc["continent_code"])
+        print(" In Eu: " + str(loc["in_eu"]))
+        print(" Postal: " + str(loc["postal"]))
+        print(" Latitude: " + str(loc["latitude"]))
+        print(" Longitude: " + str(loc["longitude"]))
+
+    except:
+        print(lr,"\n ERROR")
+        exit()
+
+
+    print(ly,"\n Finish")
---
+++ Run.py
@@ -0,0 +1,54 @@
+from colorama import Fore as color
+from time import sleep
+from Banner import banner
+from URLToIP import url_ip
+from CloudFlare import cloud_flare
+from IPLocation import ip_location
+from PortScanner import port_scanner
+from Whois import whois
+
+
+while True:
+    banner()
+
+    print(color.LIGHTYELLOW_EX,"–•[HOME]•–———————————————————————————————————————————————————————————")
+    sleep(0.05)
+    print(color.LIGHTYELLOW_EX,"\n [01] URL To IP")
+    sleep(0.05)
+    print(color.LIGHTYELLOW_EX,"[02] Cloud Flare")
+    sleep(0.05)
+    print(color.LIGHTYELLOW_EX,"[03] IP Location")
+    sleep(0.05)
+    print(color.LIGHTYELLOW_EX,"[04] Port Sccaner")
+    sleep(0.05)
+    print(color.LIGHTYELLOW_EX,"[05] Whois")
+    sleep(0.05)
+    print(color.LIGHTYELLOW_EX,"[00] Exit")
+    sleep(0.05)
+    print(color.WHITE)
+    h_input = input(" @G.O>>> ")
+
+
+    if h_input == "1" :
+        url_ip()
+
+    elif h_input == "2" :
+        cloud_flare()
+
+    elif h_input == "3" :
+        ip_location()
+
+    elif h_input == "4" :
+        port_scanner()
+
+    elif h_input == "5" :
+        whois()
+
+    elif h_input == "0" :
+        exit()
+
+    else:
+        print(color.LIGHTRED_EX,"\n ERROR")
+        exit()
+
+    input()
