f580e3f0825e654c8690df1e19c8bb80a304f006
aminreza <108486211+artorki@users.noreply.github.com>
2022-11-23 21:54:22 +0330

    Update URLToIP.py

--- URLToIP.py
+++ URLToIP.py
@@ -1,37 +1,45 @@
+# Garlic To Onion / URL To IP
+
+
 from time import sleep
 from socket import gethostbyname
 from colorama import Fore
+
 from Banner import banner
 
 
 ly,lr,lg,w = Fore.LIGHTYELLOW_EX,Fore.LIGHTRED_EX,Fore.LIGHTGREEN_EX,Fore.WHITE
 
 
-def url_ip():
+def url_ip() :
+    
     banner()
 
-    sleep(0.05)
-    print(ly,"–•[URL TO IP]•–———————————————————————————————————————————————————————————")
+    sleep (0.05)
+    print (ly , "–•[URL TO IP]•–———————————————————————————————————————————————————————————")
 
-    sleep(0.05)
-    print(ly,"\n [!] ENTER A URL:")
+    
+    sleep (0.05)
+    print (ly , "\n [~] ENTER A URL:")
 
-    sleep(0.05)
-    print(w)
-    url = input(" @G.O>>> ").lower()
+    sleep (0.05)
+    print (w)
+    url = input(" @G.O>>> ") . lower()
 
     if url == "0" :
         exit()
+        
     elif url == "" :
-        print(lr,"\n ERROR")
+        print (lr , "\n [!] ERROR")
         exit()
 
 
-    try:
+    try :
         ip = gethostbyname(url)
-        print(lg,"\n [+] IP:",ip)
+        print (lg , "\n [+] IP:" , ip)
 
-    except:
-        print(lr,"\n ERROR")
+    except :
+        print (lr , "\n [!] ERROR")
+    
     
     print(ly,"\n Finish")
