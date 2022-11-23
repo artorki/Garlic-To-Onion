1c721ee9bb6ec9f4635226b6e8b5500aa9be8746
aminreza <108486211+artorki@users.noreply.github.com>
2022-11-23 21:57:41 +0330

    Update Whois.py

--- Whois.py
+++ Whois.py
@@ -1,48 +1,57 @@
+# Garlic To Onion / Whois
+
+
 import socket
 from time import sleep
 from colorama import Fore
+
 from Banner import banner
 
 
 ly,lr,lg,w = Fore.LIGHTYELLOW_EX,Fore.LIGHTRED_EX,Fore.LIGHTGREEN_EX,Fore.WHITE
 
 
-def whois():
+def whois() :
+    
     banner()
 
-    sleep(0.05)
-    print(ly,"–•[WHOIS]•–———————————————————————————————————————————————————————————")
+    sleep (0.05)
+    print (ly , "–•[WHOIS]•–———————————————————————————————————————————————————————————")
 
-    sleep(0.05)
-    print(ly,"\n [!] ENTER A URL:")
+    
+    sleep (0.05)
+    print (ly , "\n [~] ENTER A URL:")
 
-    sleep(0.05)
-    print(w)
+    sleep (0.05)
+    print (w)
     url = input(" @G.O>>> ")
 
     if url == "0" :
         exit()
+        
     elif url == "" :
-        print(lr,"\n ERROR")
+        print (lr , "\n [!] ERROR")
         exit()
  
 
-    if url[-3:] == "org" or url[-3:] == "com" or url[-3:] == "net":
+    if url[-3:] == "org" or url[-3:] == "com" or url[-3:] == "net" :
         server = "whois.internic.net"
 
-    else:
+    else :
         server =  "whois.iana.org"
 
-    try:
+        
+    try :
         s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
         s.connect((server,43))
-        s.send((url+"\r\n").encode())
+        s.send((url+"\r\n") . encode())
         output = s.recv(10000)
 
-        print(lg,"\n " + output.decode())
+        print (lg , "\n " + output.decode())
 
-    except:
-        print(lr,"\n ERROR")
+    except :
+        print (lr , "\n [!] ERROR")
         exit()
 
-    print(ly,"\n Finish")
+        
+    print (ly , "\n Finish")
