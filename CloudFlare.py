7b12ce338be31cbadafbc8db75a8f0814a8532fc
ᴀᴍɪɴʀᴇᴢᴀ <108486211+artorki@users.noreply.github.com>
2022-11-24 17:27:27 +0330

    Update CloudFlare.py

--- CloudFlare.py
+++ CloudFlare.py
@@ -1,62 +1,66 @@
 # Garlic To Onion / Cloud Flare
 
 
-from ipapi import location
 from time import sleep
+from ipapi import location
 from socket import gethostbyname
-from colorama import Fore
 
 from Banner import banner
 
 
-ly,lr,lg,g,w = Fore.LIGHTYELLOW_EX,Fore.LIGHTRED_EX,Fore.LIGHTGREEN_EX,Fore.GREEN,Fore.WHITE
+white , cyan , green , yellow , red = "\033[0;37m" , "\033[96m" , "\033[92m" , "\033[93m" , "\033[91m"
 
 
 def cloud_flare() :
-    
-    banner()
 
-    sleep (0.05)
-    print (ly , "–•[Cloud Flare]•–———————————————————————————————————————————————————————————")
-
-    sleep (0.05)
-    print (ly , "\n [~] ENTER A URL:")
+    while True :
 
     
-    sleep (0.05)
-    print (w)
-    url = input(" @G.O>>> ") . lower()
+        banner()
 
-    if url == "0" :
-        exit()
-        
-    elif url == "" :
-        print (lr , "\n [!] ERROR")
-        exit()
-
-
-    sub_list = ["home","admin","administrator","blog","blogs","shop","downloads","webmail","mail","email","mysql","local","localhost","cpanel","ftp","direct","forums","www","www1","www2","www3","www4","www5","api","server","vpn"]
-
-    try :
         sleep (0.05)
-        print (g)
-        print (" [+] " + gethostbyname(str(url)) + " : " + url , lg)
-        
-    except :
-        print (lr , "\n [!] Error")
-        exit()
+        print (yellow , "–•[Cloud Flare]•–———————————————————————————————————————————————————————————")
 
+
+        sleep (0.05)
+        print (yellow , "\n [~] ENTER A URL:")
+    
+        sleep (0.05)
+        print (white)
+        url = input(" @G.O>>> ") . lower()
+
+        if url == "0" :
+            exit()
         
-    for i in sub_list :
+        elif url == "" :
+            print (red , "\n [!] ERROR")
+            exit()
+
+
+        sub_list = ["home","admin","administrator","blog","blogs","shop","downloads","webmail","mail","email","mysql","local","localhost","cpanel","ftp","direct","forums","www","www1","www2","www3","www4","www5","api","server","vpn"]
 
         try :
-            host = str(i) + "." + url
-            ip = gethostbyname(str(host))
-            loc = location(ip)
-            print (" [+] " + loc["country_name"] + " : " + ip + " : " + host)
+            sleep (0.05)
+            print (green)
+            print (" [+] " + gethostbyname(str(url)) + " : " + url)
+        
+        except :
+            print (red , "\n [!] Error")
+            exit()
+
+        
+        for i in sub_list :
+
+            try :
+                host = str(i) + "." + url
+                ip = gethostbyname (str(host))
+                loc = location (ip)
+                print (" [+] " + loc["country_name"] + " : " + ip + " : " + host)
             
-        except:
-            pass
+            except :
+                pass
     
     
-    print (ly , "\n Finish")
+        print (yellow , "\n Finish")
+
+        input()
