ac9b0a7232fbf5f3d089e27cc0341b5b130a9726
ᴀᴍɪɴʀᴇᴢᴀ <108486211+artorki@users.noreply.github.com>
2022-11-24 19:20:56 +0330

    Update PortScanner.py

--- PortScanner.py
+++ PortScanner.py
@@ -3,66 +3,69 @@
 
 import nmap
 from time import sleep
-from colorama import Fore
 
 from Banner import banner
 
 
-ly,lr,lg,w = Fore.LIGHTYELLOW_EX,Fore.LIGHTRED_EX,Fore.LIGHTGREEN_EX,Fore.WHITE
+white , green , yellow , red = "\033[0;37m" , "\033[92m" , "\033[93m" , "\033[91m"
 
 
 def port_scanner() :
-    
+
+    while True :
+   
+ 
     banner()
 
     sleep (0.05)
-    print (ly , "–•[PORT SCANNER]•–———————————————————————————————————————————————————————————")
-
-    sleep (0.05)
-    print (ly , "\n [!] ENTER A IP:")
+    print (yellow , "–•[PORT SCANNER]•–———————————————————————————————————————————————————————————")
 
     
     sleep (0.05)
-    print (w)
-    ip = input(" @G.O>>> ")
+    print (yellow , "\n [~] ENTER A IP:")
+
+    sleep (0.05)
+    print (white)
+    ip = input (" @G.O>>> ")
 
     if ip == "0" :
-        exit()
+        break
         
     elif ip == "" :
-        print (lr , "\n [!] ERROR")
+        print (red , "\n [!] ERROR")
         exit()
 
         
     sleep (0.05)
-    print (ly,"\n [!] ENTER PORT RANGE:")
+    print (yellow , "\n [~] ENTER PORT RANGE:")
 
-    
     sleep (0.05)
-    print (w)
-    port = input(" @G.O>>> ")
+    print (white)
+    port = input (" @G.O>>> ")
 
     if port == "0" :
-        exit()
+        break
         
     elif port == "" :
-        print (lr , "\n [!] ERROR")
+        print (red , "\n [!] ERROR")
         exit()
 
 
     try :
         
         scanner = nmap.PortScanner()
-        scanner.scan(ip,port)
+        scanner.scan (ip , port)
         tcp = scanner[ip]["tcp"].keys()
 
-        for i in tcp:
+        for i in tcp :
             service = scanner[ip]["tcp"][i]["name"]
-            print (lg , " [+] Port {} - {} is Open".format(i,service))
+            print (green , " [+] Port {} - {} is Open" . format (i , service))
 
     except :
-        print (lr , "\n ERROR")
+        print (red , "\n [!] ERROR")
         exit()
 
         
-    print (ly , "\n Finish")
+    print (yellow , "\n Finish")
+
+    input()
