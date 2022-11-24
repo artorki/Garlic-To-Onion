0c0bc124d335dd68bdc349c8da28b0d295a2aee9
ᴀᴍɪɴʀᴇᴢᴀ <108486211+artorki@users.noreply.github.com>
2022-11-24 19:28:02 +0330

    Update PortScanner.py

--- PortScanner.py
+++ PortScanner.py
@@ -15,57 +15,57 @@ def port_scanner() :
     while True :
    
  
-    banner()
+        banner()
 
-    sleep (0.05)
-    print (yellow , "–•[PORT SCANNER]•–———————————————————————————————————————————————————————————")
+        sleep (0.05)
+        print (yellow , "–•[PORT SCANNER]•–———————————————————————————————————————————————————————————")
 
     
-    sleep (0.05)
-    print (yellow , "\n [~] ENTER A IP:")
+        sleep (0.05)
+        print (yellow , "\n [~] ENTER A IP:")
 
-    sleep (0.05)
-    print (white)
-    ip = input (" @G.O>>> ")
+        sleep (0.05)
+        print (white)
+        ip = input (" @G.O>>> ")
 
-    if ip == "0" :
-        break
+        if ip == "0" :
+            break
         
-    elif ip == "" :
-        print (red , "\n [!] ERROR")
-        exit()
-
-        
-    sleep (0.05)
-    print (yellow , "\n [~] ENTER PORT RANGE:")
-
-    sleep (0.05)
-    print (white)
-    port = input (" @G.O>>> ")
-
-    if port == "0" :
-        break
-        
-    elif port == "" :
-        print (red , "\n [!] ERROR")
-        exit()
-
-
-    try :
-        
-        scanner = nmap.PortScanner()
-        scanner.scan (ip , port)
-        tcp = scanner[ip]["tcp"].keys()
-
-        for i in tcp :
-            service = scanner[ip]["tcp"][i]["name"]
-            print (green , " [+] Port {} - {} is Open" . format (i , service))
-
-    except :
-        print (red , "\n [!] ERROR")
-        exit()
+        elif ip == "" :
+            print (red , "\n [!] ERROR")
+            exit()
 
         
-    print (yellow , "\n Finish")
+        sleep (0.05)
+        print (yellow , "\n [~] ENTER PORT RANGE:")
 
-    input()
+        sleep (0.05)
+        print (white)
+        port = input (" @G.O>>> ")
+
+        if port == "0" :
+            break
+        
+        elif port == "" :
+            print (red , "\n [!] ERROR")
+            exit()
+
+
+        try :
+        
+            scanner = nmap.PortScanner()
+            scanner.scan (ip , port)
+            tcp = scanner[ip]["tcp"].keys()
+
+            for i in tcp :
+                service = scanner[ip]["tcp"][i]["name"]
+                print (green , " [+] Port {} - {} is Open" . format (i , service))
+
+        except :
+            print (red , "\n [!] ERROR")
+            exit()
+
+        
+        print (yellow , "\n Finish")
+
+        input()
