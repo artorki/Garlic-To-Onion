ea4b74ebdfbc2fc6badee778b570f91abc4b8a26
ᴀᴍɪɴʀᴇᴢᴀ <108486211+artorki@users.noreply.github.com>
2022-11-24 19:32:13 +0330

    Create Libs.py

---
+++ Libs.py
@@ -0,0 +1,41 @@
+# Garlic To Onion / Libs 
+  
+  
+ from Banner import banner 
+  
+  
+ red = '\033[91m' 
+  
+          
+ try :   
+     import ipapi 
+      
+ except : 
+     banner() 
+     print (red , '[!] Pleas Install ipapi') 
+      
+      
+ try : 
+     import requests 
+      
+ except : 
+     banner() 
+     print (red , '[!] Pleas Install requests') 
+      
+      
+ try : 
+     import socket 
+      
+ except : 
+     banner() 
+     print (red , '[!] Pleas Install socket') 
+
+try : 
+     import nmap
+      
+ except : 
+     banner() 
+     print (red , '[!] Pleas Install nmap')
+  
+
+ print()
