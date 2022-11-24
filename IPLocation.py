5f0ef6fd4085bf1340f0a2b18cdb94e28d61e033
ᴀᴍɪɴʀᴇᴢᴀ <108486211+artorki@users.noreply.github.com>
2022-11-24 17:46:17 +0330

    Update IPLocation.py

--- IPLocation.py
+++ IPLocation.py
@@ -2,73 +2,78 @@
 
 
 from time import sleep
-from colorama import Fore
 from ipapi import location
 
 from Banner import banner
 
 
-ly,lr,g,lg,w = Fore.LIGHTYELLOW_EX,Fore.LIGHTRED_EX,Fore.GREEN,Fore.LIGHTGREEN_EX,Fore.WHITE
+white , cyan , green , yellow , red = "\033[0;37m" , "\033[96m" , "\033[92m" , "\033[93m" , "\033[91m"
 
 
 def ip_location() :
     
-    banner()
+    while True :
 
-    sleep (0.05)
-    print (ly , "–•[IP LOCATION]•–———————————————————————————————————————————————————————————")
 
-    sleep (0.05)
-    print (ly , "\n [~] ENTER A IP:")
+        banner()
 
+        sleep (0.05)
+        print (yellow , "–•[IP LOCATION]•–———————————————————————————————————————————————————————————")
+
+
+        sleep (0.05)
+        print (yellow , "\n [~] ENTER A IP:")
     
-    sleep (0.05)
-    print (w)
-    ip = input(" @G.O>>> ")
+        sleep (0.05)
+        print (white)
+        ip = input (" @G.O>>> ")
 
-    if ip == "0" :
-        exit()
+        if ip == "0" :
+            break
         
-    elif ip == "" :
-        print (lr , "\n [!] ERROR")
-        exit()
-
-        
-    try :
-        loc = location(ip)
-
-        print (g , "\n [+] IP:")
-        print (lg , "IP: " + loc["ip"])
-        print (" Network: " + loc["network"])
-        print (" Version: " + loc["version"])
-
-        print (g , "[+] Location:")
-        print (lg , "Country: " + loc["country_name"])
-        print (" Center: " + loc["country_capital"])
-        print (" City: " + loc["city"])
-        print (" Region: " + loc["region"])
-
-        print (g , "[+] Country Information:")
-        print (lg , "Languages: " + loc["languages"])
-        print (" Timezone: " + loc["timezone"])
-        print (" Calling Code: " + loc["country_calling_code"])
-        print (" Utc Offset: " + loc["utc_offset"])
-        print (" Country Tld: " + loc["country_tld"])
-        print (" Currency: " + loc["currency"])
-        print (" Currency Name: " + loc["currency_name"])
-        print (" Region Code: " + loc["region_code"])
-        print (" Country Code: " + loc["country_code"])
-        print (" Country Code iso3: " + loc["country_code_iso3"])
-        print (" Continent Code: " + loc["continent_code"])
-        print (" In Eu: " + str(loc["in_eu"]))
-        print (" Postal: " + str(loc["postal"]))
-        print (" Latitude: " + str(loc["latitude"]))
-        print (" Longitude: " + str(loc["longitude"]))
+        elif ip == "" :
+            print (red , "\n [!] ERROR")
+            exit()
 
         
-    except :
-        print (lr , "\n [!] ERROR")
-        exit()
+        try :
+
+            loc = location (ip)
+
+            print (green , "\n [+] IP:")
+            print ("IP: " + loc["ip"])
+            print (" Network: " + loc["network"])
+            print (" Version: " + loc["version"])
+
+            print (green , "[+] Location:")
+            print ("Country: " + loc["country_name"])
+            print (" Center: " + loc["country_capital"])
+            print (" City: " + loc["city"])
+            print (" Region: " + loc["region"])
+
+            print (green , "[+] Country Information:")
+            print ("Languages: " + loc["languages"])
+            print (" Timezone: " + loc["timezone"])
+            print (" Calling Code: " + loc["country_calling_code"])
+            print (" Utc Offset: " + loc["utc_offset"])
+            print (" Country Tld: " + loc["country_tld"])
+            print (" Currency: " + loc["currency"])
+            print (" Currency Name: " + loc["currency_name"])
+            print (" Region Code: " + loc["region_code"])
+            print (" Country Code: " + loc["country_code"])
+            print (" Country Code iso3: " + loc["country_code_iso3"])
+            print (" Continent Code: " + loc["continent_code"])
+            print (" In Eu: " + str(loc["in_eu"]))
+            print (" Postal: " + str(loc["postal"]))
+            print (" Latitude: " + str(loc["latitude"]))
+            print (" Longitude: " + str(loc["longitude"]))
+
+        
+        except :
+            print (red , "\n [!] ERROR")
+            exit()
 
 
-    print (ly , "\n Finish")
+        print (yellow , "\n Finish")
+
+        input()
