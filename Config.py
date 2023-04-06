af9a81a111096544b636a92eaf344437090aaa25
ᴀᴍɪɴʀᴇᴢᴀ <108486211+artorki@users.noreply.github.com>
2023-04-06 04:54:17 +0330

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,5 @@
+# Garlic to Onion V3 ARTorki
+
+time_out = 10
+SubList = ["","home","admin","administrator","blog","blogs","shop","downloads","webmail","mail","email","mysql","local","localhost","cpanel","ftp","direct","forums","www","www1","www2","www3","www4","www5","api","server","vpn"]
+Port = [20, 21, 22, 23, 25, 53, 80, 443, 2082, 2083, 2086, 2087, 2222, 3389, 8080, 8443]
